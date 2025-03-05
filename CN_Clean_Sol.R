4deaf6b8d0782c348f2f7ea2c727705e26d55e27
SolomonMM876 <s.maerowitz-mcmahan@westernsydney.edu.au>
2025-03-05 17:22:39 +1100

    CN_data_clean

---
+++ CN_Clean_Sol.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(readxl)
+
+#results of CN spiking
+CN_samps<- read_excel("raw/CNH Sample weights_SMM-spiked-Amaia's result.xlsx")
+
+CNH_weights <- read_excel("raw/CNH Sample weights_spike_w.xlsx", , 
+                                 sheet = "Amaia_Samples")
+#select parts of df I care about
+sample_w<-CNH_weights%>%select(1:3)%>%filter(!is.na(Sample_ID))
+
+#make df pretty
+spike_w<-CNH_weights%>%select(1,2,10:12)%>%rename(CO3_mg=Obtained, NH4_mg= `...11`, total = `...12`)%>%filter(!is.na(CO3_mg)&!CO3_mg=='C')%>%
+  mutate(CO3_mg=as.numeric(CO3_mg),NH4_mg=as.numeric(NH4_mg))
+
+#calc percent of N and C in spiked samples
+#first sample removed because potential contam from previous analysis
+CO3_mean_N_perc<-CN_samps%>%filter(name=='NaHCO3'&!weight=='6.95')%>% summarise(mean(`N%`) )%>%pull()
+CO3_mean_C_perc<-CN_samps%>%filter(name=='NaHCO3'&!weight=='6.95')%>% summarise(mean(`C%`) )%>%pull()
+NH4_mean_N_perc<-CN_samps%>%filter(name=='(NH)42SO4'&!weight=='6.799')%>% summarise(mean(`N%`) )%>%pull()
+NH4_mean_C_perc<-CN_samps%>%filter(name=='(NH)42SO4'&!weight=='6.799')%>% summarise(mean(`C%`) )%>%pull()
+
+#calc Total spike for C and N
+CN_samps<-spike_w%>%filter(!is.na(Plate_ID))%>%
+  mutate(Carb_spike= (CO3_mg*CO3_mean_C_perc/100)+(NH4_mg*NH4_mean_C_perc/100),#total carbon spike
+         Nitr_spike= (CO3_mg*CO3_mean_N_perc/100)+(NH4_mg*NH4_mean_N_perc/100))%>%#total nitrogen spike
+left_join(CN_samps, by=c('Plate_ID'='name'))%>%
+  mutate(Samp_N= `N mg`-Nitr_spike,
+         Samp_C= `C mg`-Carb_spike)%>%
+  left_join(sample_w)%>%
+  mutate(perc_N_Samp= (Samp_N/weight_mg)*100,
+         perc_C_Samp= (Samp_C/weight_mg)*100)
+
+#I would say about half of these are 
